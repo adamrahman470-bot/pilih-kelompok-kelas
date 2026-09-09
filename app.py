25a677465f5f34cfe53759314e6cba87f2a0228c
Adam Rahman Dara Anjani <adamrahman470@gmail.com>
2026-09-09 23:03:47 +0700

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ st.markdown("<p style='text-align: center; color: #9CA3AF; margin-bottom: 0px;'>
 
 st.markdown("""
     <div style='text-align: center; margin-bottom: 25px;'>
-        <span class='dev-badge'>⚡ Designed & Developed by <b>Adam Rahman D.A</b></span>
+        <span class='dev-badge'>⚡ Designed & Developed by <b>Adam Rahman D.A. S.T.</b></span>
     </div>
 """, unsafe_allow_html=True)
 
